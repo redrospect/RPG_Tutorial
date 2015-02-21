73335a15b22a6887b228c4e41296ab5073988663
redrospect <twikii@live.com>
2015-02-20 19:30:36 -0700

    8

---
+++ Assets/Standard Assets/MyScripts/Enemy_AI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_AI : MonoBehaviour {
+	public Transform target;
+	public int moveSpeed;
+	public int rotationSpeed;
+
+	private Transform myTransform;
+
+	void Awake() {
+		myTransform = transform; //transform from unity
+	//happens before anything else happens in the script
+	}
+	// Use this for initialization
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag ("Player"); //when you select your player in unity, the tag is 'player' --that's where this is pointing to (can be seen in inspector
+
+		target = go.transform;
+
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine(target.position, myTransform.position, Color.yellow); //draws a line from the enemy to the player (didn't work?)
+	//look at target
+		myTransform.rotation = Quaternion.Slerp (myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+	//move towards target
+		myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+		//time.deltatime normalizes movement speed between systems
+	
+	}
+}
---
+++ Assets/Standard Assets/MyScripts/Enemy_Health.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Health : MonoBehaviour {
+	public int maxHealth = 100;
+	public int currentHealth = 100;
+	
+	public float healthBarLength;
+	
+	// Use this for initialization
+	void Start () {
+		healthBarLength = Screen.width / 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		adjustCurrentHealth (0);
+	}
+	
+	void OnGUI() {
+		GUI.Box(new Rect (10,40, healthBarLength, 20), currentHealth + "/" + maxHealth);
+	}
+	public void adjustCurrentHealth(int adj) {
+		currentHealth += adj;  //add to current health value of adj. ex-heal +20, add 20 to current health, or -20 if damage is taken
+		
+		if (currentHealth < 1)
+			currentHealth = 0;
+		if (currentHealth > maxHealth)
+			currentHealth = maxHealth;
+		if (maxHealth < 1)
+			maxHealth = 1;
+		
+		healthBarLength = (Screen.width / 2) * (currentHealth / (float)maxHealth); //bar length
+	}
+}
